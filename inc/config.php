ca0cecb992e5f7b4058f05ee3e5c5f5d350b8a18
Allen Garvey <allengarvey@hotmail.com>
2015-02-16 23:47:50 -0500

    add constants and includes for greater renaming flexbility

--- inc/config.php
+++ inc/config.php
@@ -3,3 +3,8 @@
 //config to run on localhost
 define("BASE_URL","/Websites/allen_garvey_2/");
 define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/Websites/allen_garvey_2/");
+
+define("CODE_URL", BASE_URL.'code/');
+define("VIDEO_URL", BASE_URL.'video/');
+define("ARTWORK_URL", 'https://norwalk.digication.com/allengarvey/Welcome/published');
+define("MUSIC_URL", 'https://myspace.com/allengarvey/music/songs');
