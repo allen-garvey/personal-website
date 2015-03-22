287cf26745942bd1f51e8b29240b8732e9c92c44
Allen Garvey <allengarvey@hotmail.com>
2015-03-21 22:58:24 -0400

    revise body copy on homepage and styles for xs screen sizes

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
 	include(ROOT_PATH.'inc/head.php');
 	$title = 'home'; //for nav-bar active class
 ?>
-<body>
-
-<header class='jumbotron'>
+<header class='jumbotron home'>
 	<?php include(ROOT_PATH.'inc/navbar.php'); ?>
 	<div class='container main_brand'>
 		<h1 class='logo_text'>Allen Garvey</h1>
@@ -20,7 +18,7 @@
 				<p>I&#8217;m a software developer, designer and musician. As far back as I can remember I&#8217;ve always been intensely curious, which leads me to never stop learning. I believe that if you do the work and put in the time for the things that you are most passionate about, good things will come to you.</p>
 			</div>
 			<div class='col-sm-4'>
-				<p>Some highlights so far: I&#8217;ve <strong>written an opera</strong> while completing a degree in music composition, <strong>performed all over the world,</strong> changed track to study computer science, <strong>taught web development</strong> and have <strong>created all the illustrations and graphics</strong> on this site.</p>
+				<p>Some highlights so far: I&#8217;ve <strong>written an opera</strong> while completing a degree in music composition, <strong>performed all over the world,</strong> changed track to study computer science, <strong>taught web development</strong> to high-schoolers and have <strong>created all the illustrations and graphics</strong> on this site.</p>
 			</div>
 			<div class='col-sm-4'>
 				<img src="<?= BASE_URL.'images/allen1.jpg'; ?>" alt="Allen Garvey portrait illustration" height='150px' width='150px' />
@@ -34,18 +32,18 @@
 			<div class='row'>
 				<div class='col-sm-6'>
 					<h3><a href="<?= CODE_URL; ?>">Code</a></h3>
-					<p>Some programs and websites I&#8217;ve made.</p>
+					<p class='sm_col_desc'>Some apps and websites I&#8217;ve made.</p>
 				</div>
 				<div class='col-sm-6'>
 					<img class='icon' src='<?= BASE_URL.'images/icons/code-icon.png'; ?>'/>
 				</div>
 			</div>
 		</div>
-		<div class='col-sm-6'>
+		<div class='col-sm-6 top_margin_sm_max'>
 			<div class='row'>
 				<div class='col-sm-6'>
 					<h3><a href="<?= ARTWORK_URL; ?>">Artwork</a></h3>
-					<p>Some of my artwork.</p>
+					<p class='sm_col_desc'>Some of my artwork, illustrations and designs.</p>
 				</div>
 				<div class='col-sm-6'>
 					<img class='icon' src='<?= BASE_URL.'images/icons/art-icon.png'; ?>'/>
@@ -54,23 +52,23 @@
 		</div>
 	</div>
 
-	<div class='row top_buffer_sm'>
+	<div class='row top_buffer_sm top_margin_sm_max'>
 		<div class='col-sm-6'>
 			<div class='row'>
 				<div class='col-sm-6'>
 					<h3><a href="<?= MUSIC_URL; ?>">Music</a></h3>
-					<p>Some music I&#8217;ve written.</p>
+					<p class='sm_col_desc'>Some music I&#8217;ve written.</p>
 				</div>
 				<div class='col-sm-6'>
 					<img class='icon' src='<?= BASE_URL.'images/icons/music-icon.png'; ?>'/>
 				</div>
 			</div>
 		</div>
-		<div class='col-sm-6'>
+		<div class='col-sm-6 top_margin_sm_max'>
 			<div class='row'>
 				<div class='col-sm-6'>
 					<h3><a href="<?= VIDEO_URL; ?>">Video</a></h3>
-					<p>Some videos I&#8217;ve made.</p>
+					<p class='sm_col_desc'>Some videos I&#8217;ve made.</p>
 				</div>
 				<div class='col-sm-6'>
 					<img class='icon' src='<?= BASE_URL.'images/icons/video-icon.png'; ?>'/>
