510f7571013da9a33da4d157f757908a6b65a749
Allen Garvey <allengarvey@hotmail.com>
2015-05-19 22:48:46 -0400

    fix footer so it's properly centered

--- inc/footer_text.php
+++ inc/footer_text.php
@@ -1,5 +1,5 @@
 <script type="text/javascript" src="<?= BASE_URL.'scripts/email.js'; ?>"></script>
 <script type="text/javascript">
 var email_scrambler = new email_scrambler();
-document.write("<footer class='container'>" + email_scrambler.get_email_link() + "</footer>");
+document.write("<footer>" + email_scrambler.get_email_link() + "</footer>");
 </script>
