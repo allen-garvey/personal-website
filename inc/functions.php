d4ecea3d546c85bca8dcfd4e2dadc24eeb46077a
Allen Garvey <allengarvey@hotmail.com>
2015-05-19 23:25:20 -0400

    delete unused functions in functions.php and add function to format html text with correct character entities

--- inc/functions.php
+++ inc/functions.php
@@ -1,40 +1,23 @@
 <?php
-function get_stylesheets(){
-	$stylesheets = 
-		"<link rel='stylesheet' type='text/css' href='http://www.allengarvey.com/style.css'>" . 
-		"<link href='http://fonts.googleapis.com/css?family=Sorts+Mill+Goudy' rel='stylesheet' type='text/css'>" .
-		"<link href='http://fonts.googleapis.com/css?family=Vollkorn' rel='stylesheet' type='text/css'>";
+//replaces dumb quotes with smart quotes, and -- and --- with en and em dash character codes, respectively
+function format_html_text($unformatted_text){
+	$left_smart_single_quote = '&#8216;';
+	$right_smart_single_quote = '&#8217;';
+	$left_smart_double_quote = '&#8220;';
+	$right_smart_double_quote = '&#8221;';
+	$en_dash = '&#8211;';
+	$em_dash = '&#8212;';
 
-	return $stylesheets;
-}
+	$unformatted_text = preg_replace('/\\B"\\b/', $left_smart_double_quote, $unformatted_text);
+	$unformatted_text = str_replace('"', $right_smart_double_quote, $unformatted_text);
+	$unformatted_text = preg_replace('/\\B\'\\b/', $left_smart_single_quote, $unformatted_text);
+	$unformatted_text = str_replace("'", $right_smart_single_quote, $unformatted_text);
 
-function get_metadata(){
-	$metadata = 
-		"<meta name='Description' content='Personal web page of bass player and composer Allen Garvey'>" .
-		"<meta name='Keywords' content='music, musician, bass, electric bass, acoustic bass, composer, cruise ship, cruise ship musician, film music'>".
-		"<meta name=viewport content='width=device-width, initial-scale=1'>";
-
-	return $metadata;
-}
-
-function get_titlebar(){
-	$titlebar = "<h1 class='title'>" . 
-		"<div class='menu_box'><a class='title' href='http://www.allengarvey.com/index.php'>Home</a></div>" . 
-		"<div class='menu_box'><a class='title' href='http://www.allengarvey.com/interactive/index.php#center'>Interactive</a></div>" .
-		"<div class='menu_box'><a class='title' href='http://www.allengarvey.com/music.php#center'>Music</a></div>" .
-		"<div class='menu_box'><a class='title' href='http://www.allengarvey.com/video.php#center'>Video</a></div>" .
-		"<div class='menu_box'><a class='title' href='http://www.allengarvey.com/contact.php#center'>Contact</a></div>" .
-		"</h1>";
-
-	return $titlebar;
-}
-
-function get_title(){
-	return "Allen Garvey | Musician";
+	$formatted_text = str_replace('---', $em_dash, $unformatted_text);
+	$unformatted_text = str_replace('--', $en_dash, $unformatted_text);
+	return $formatted_text;
 }
 
 
 
 
-
-?>
