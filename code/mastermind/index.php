39dce0893520881e622aa80900e2fd761367bc0c
Allen Garvey <allengarvey@hotmail.com>
2015-05-27 20:52:04 -0400

    fix unnecessary whitespace in mastermind index caused by redundant html tags

--- code/mastermind/index.php
+++ code/mastermind/index.php
@@ -1,12 +1,11 @@
 <?php 
 	require_once("../../inc/config.php");
-	include(ROOT_PATH.'inc/head.php');
+	include(ROOT_PATH.'inc/views/head_only.php');
 ?>
 <body>
 <link rel="stylesheet" type="text/css" href="mastermind_style.css">
-<?php include(ROOT_PATH.'inc/navbar.php'); ?>
+<?php include(ROOT_PATH.'inc/views/navbar.php'); ?>
 <div id="top" class="center2">
-	<br>
 	<script src="mastermind.js"></script>
 </div>
 </body>
