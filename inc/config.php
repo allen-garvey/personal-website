ea0344ad0d2437bea74dd00583bcd1d0495e5133
Allen Garvey <allengarvey@hotmail.com>
2015-05-27 20:53:09 -0400

    add image dir constant to config.php

--- inc/config.php
+++ inc/config.php
@@ -8,7 +8,8 @@ define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/Websites/allen_garvey_2/");
 // define("BASE_URL","http://allengarvey.com/");
 // define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/");
 
+define("IMAGE_URL", BASE_URL.'images/');
 define("CODE_URL", BASE_URL.'code/');
 define("VIDEO_URL", BASE_URL.'video/');
-define("ARTWORK_URL", 'https://norwalk.digication.com/allengarvey/Welcome/published');
-define("MUSIC_URL", 'https://myspace.com/allengarvey/music/songs');
+define("ARTWORK_URL", BASE_URL.'artwork/');
+define("MUSIC_URL", BASE_URL.'music/');
