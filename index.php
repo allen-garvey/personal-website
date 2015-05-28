d4b2c09c7eedb27b625edd86de71936aba150b33
Allen Garvey <allengarvey@hotmail.com>
2015-05-27 23:59:55 -0400

    add articles and sections to home index page to make more semantic; restructure highlights as a list; revise copy on section descriptions

--- index.php
+++ index.php
@@ -10,72 +10,81 @@
 		<p class='lead'>Software Developer, Designer, Musician</p>
 	</div>
 </header>
-<div class='jumbotron about'>
-	<div class='container about_me'>
-		<h2>Hi, I&#8217;m Allen.</h2>
+<main>
+	<article class='jumbotron about'>
+		<div class='container about_me'>
+			<h2>Hi, I&#8217;m Allen.</h2>
+			<div class='row'>
+				<section class='col-sm-4'>
+					<p>I&#8217;m a software developer, designer and musician. As far back as I can remember I&#8217;ve always been intensely curious, which leads me to never stop learning. I believe that if you do the work and put in the time for the things that you are most passionate about, good things will come to you.</p>
+				</section>
+				<section class='col-sm-4'>
+					<h4>Some highlights so far:</h4>
+					<ul class='highlights_list'>
+						<li>wrote and produced an opera based on <em>Brave New World</em></li>
+						<li>performed in over 50 countries on 4 continents</li>
+						<li>climbed Mt. Pahia in Bora Bora on three separate occasions</li>
+						<li>taught web development, web design and Android app development to high school and college students</li>
+						<li>had artwork chosen for the <em>2015 Stacy M. Israel Art, Architecture + Design Student Exhibition</em> at Norwalk Community College</li>
+					</ul>
+				</section>
+				<div class='col-sm-4'>
+					<img src="<?= BASE_URL.'images/allen1.jpg'; ?>" alt="Allen Garvey portrait illustration" height='150' width='150' />
+				</div>
+			</div>
+		</div>
+	</article>
+	<article class='container'>
 		<div class='row'>
-			<div class='col-sm-4'>
-				<p>I&#8217;m a software developer, designer and musician. As far back as I can remember I&#8217;ve always been intensely curious, which leads me to never stop learning. I believe that if you do the work and put in the time for the things that you are most passionate about, good things will come to you.</p>
-			</div>
-			<div class='col-sm-4'>
-				<p>Some highlights so far: I&#8217;ve <strong>written an opera</strong> while completing a degree in music composition, <strong>performed all over the world,</strong> changed track to study computer science, <strong>taught web development</strong> to high-schoolers and have <strong>created all the illustrations and graphics</strong> on this site.</p>
-			</div>
-			<div class='col-sm-4'>
-				<img src="<?= BASE_URL.'images/allen1.jpg'; ?>" alt="Allen Garvey portrait illustration" height='150' width='150' />
-			</div>
+			<section class='col-sm-6'>
+				<div class='row'>
+					<div class='col-sm-6'>
+						<h3><a href="<?= CODE_URL; ?>">Code</a></h3>
+						<p class='sm_col_desc'>Websites, web applications and Mac, iOS and Android apps</p>
+					</div>
+					<div class='col-sm-6'>
+						<img class='icon' alt='' src='<?= BASE_URL.'images/icons/code-icon.png'; ?>'/>
+					</div>
+				</div>
+			</section>
+			<section class='col-sm-6 top_margin_sm_max'>
+				<div class='row'>
+					<div class='col-sm-6'>
+						<h3><a href="<?= ARTWORK_URL; ?>">Artwork</a></h3>
+						<p class='sm_col_desc'>Photos, illustrations and digital art</p>
+					</div>
+					<div class='col-sm-6'>
+						<img class='icon' alt='' src='<?= BASE_URL.'images/icons/art-icon.png'; ?>'/>
+					</div>
+				</div>
+			</section>
 		</div>
-	</div>
-</div>
-<main class='container'>
-	<div class='row'>
-		<div class='col-sm-6'>
-			<div class='row'>
-				<div class='col-sm-6'>
-					<h3><a href="<?= CODE_URL; ?>">Code</a></h3>
-					<p class='sm_col_desc'>Some apps and websites I&#8217;ve made.</p>
-				</div>
-				<div class='col-sm-6'>
-					<img class='icon' alt='' src='<?= BASE_URL.'images/icons/code-icon.png'; ?>'/>
-				</div>
-			</div>
-		</div>
-		<div class='col-sm-6 top_margin_sm_max'>
-			<div class='row'>
-				<div class='col-sm-6'>
-					<h3><a href="<?= ARTWORK_URL; ?>">Artwork</a></h3>
-					<p class='sm_col_desc'>Some of my artwork, illustrations and designs.</p>
-				</div>
-				<div class='col-sm-6'>
-					<img class='icon' alt='' src='<?= BASE_URL.'images/icons/art-icon.png'; ?>'/>
-				</div>
-			</div>
-		</div>
-	</div>
 
-	<div class='row top_buffer_sm top_margin_sm_max'>
-		<div class='col-sm-6'>
-			<div class='row'>
-				<div class='col-sm-6'>
-					<h3><a href="<?= MUSIC_URL; ?>">Music</a></h3>
-					<p class='sm_col_desc'>Some music I&#8217;ve written.</p>
+		<div class='row top_buffer_sm top_margin_sm_max'>
+			<section class='col-sm-6'>
+				<div class='row'>
+					<div class='col-sm-6'>
+						<h3><a href="<?= MUSIC_URL; ?>">Music</a></h3>
+						<p class='sm_col_desc'>Compositions and arrangements in jazz, classical, electronic and eclectic styles</p>
+					</div>
+					<div class='col-sm-6'>
+						<img class='icon' alt='' src='<?= BASE_URL.'images/icons/music-icon.png'; ?>'/>
+					</div>
 				</div>
-				<div class='col-sm-6'>
-					<img class='icon' alt='' src='<?= BASE_URL.'images/icons/music-icon.png'; ?>'/>
+			</section>
+			<section class='col-sm-6 top_margin_sm_max'>
+				<div class='row'>
+					<div class='col-sm-6'>
+						<h3><a href="<?= VIDEO_URL; ?>">Video</a></h3>
+						<p class='sm_col_desc'>Some videos I&#8217;ve made.</p>
+					</div>
+					<div class='col-sm-6'>
+						<img class='icon' alt='' src='<?= BASE_URL.'images/icons/video-icon.png'; ?>'/>
+					</div>
 				</div>
-			</div>
+			</section>
 		</div>
-		<div class='col-sm-6 top_margin_sm_max'>
-			<div class='row'>
-				<div class='col-sm-6'>
-					<h3><a href="<?= VIDEO_URL; ?>">Video</a></h3>
-					<p class='sm_col_desc'>Some videos I&#8217;ve made.</p>
-				</div>
-				<div class='col-sm-6'>
-					<img class='icon' alt='' src='<?= BASE_URL.'images/icons/video-icon.png'; ?>'/>
-				</div>
-			</div>
-		</div>
-	</div>
+	</article>
 </main>
 
 <?php include(ROOT_PATH.'inc/views/footer.php'); ?>
