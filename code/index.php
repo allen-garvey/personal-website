aba9bdb395c96d93645b2ec13b88c96873c876ff
Allen Garvey <allengarvey@hotmail.com>
2015-05-30 02:03:50 -0400

    add explode class to section links on code homepage

--- code/index.php
+++ code/index.php
@@ -34,7 +34,7 @@
 				if($i > 0 && $i % $items_per_row === 0){
 					echo "</div><div class='row top_buffer_sm'>";
 				}
-				echo "<div class='col-md-3 bottom_margin_sm'><a href=$entry[link] class='code_link'><h3>$entry[title]</h3><figure><img src='$icon_url' alt='$alt' /></figure></a><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				echo "<div class='col-md-3 bottom_margin_sm'><a href=$entry[link] class='code_link explode'><h3>$entry[title]</h3><figure><img src='$icon_url' alt='$alt' /></figure></a><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
