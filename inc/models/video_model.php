7672d9e3904c2f5585235205f00fd79296702acb
Allen Garvey <allengarvey@hotmail.com>
2015-05-30 23:53:20 -0400

    fix typo in video_model

--- inc/models/video_model.php
+++ inc/models/video_model.php
@@ -6,7 +6,7 @@ function videos(){
 	$videos[] = array('title' => 'The Last Battle', 
 						'release_date' => '01/28/2014',
 						'url' => 'http://www.youtube-nocookie.com/embed/WEsaplY1yco', 
-					'description' => 'A re-scoring of a clip from Final Fantasy XIV. This dramatic scene was both fun challenging to score. 
+					'description' => 'A re-scoring of a clip from Final Fantasy XIV. This dramatic scene was both fun and challenging to score. 
 			It ended up being a showcase for my eclectic musical taste---I managed to use orchestral, funk, dubstep, downtempo, and a little polytonality all in the same track.');
 	$videos[] = array('title' => 'Marching Orders', 
 						'release_date' => '01/05/2014',
