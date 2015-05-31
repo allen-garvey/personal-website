6eadf1e9a16bcc16d90d80f4afa41ebb24a09eb6
Allen Garvey <allengarvey@hotmail.com>
2015-05-31 01:44:57 -0400

    add thumbnail for schedule monster 2

--- inc/models/code_model.php
+++ inc/models/code_model.php
@@ -10,7 +10,7 @@ function code_model(){
 	$code_model[] = ['title' => 'Perfect Pitch Trainer', 'link' => CODE_URL.'perfect_pitch/', 'release_date' => '12/20/2014', 'icon_url' => 'perfect-pitch-trainer-icon.png'];
 	// $code_model[] = ['title' => 'Shape Calculator', 'link' => CODE_URL.'shape_calculator/', 'release_date' => '09/28/2014', 'icon_url' => ''];
 	$code_model[] = ['title' => 'Countdowner', 'link' => 'https://github.com/allen-garvey/countdowner/', 'release_date' => '06/23/2014', 'icon_url' => 'countdowner-icon.png'];
-	$code_model[] = ['title' => 'Schedule Monster 2', 'link' => CODE_URL.'schedule_monster2/', 'release_date' => '06/09/2014', 'icon_url' => 'http://assets-s3.usmagazine.com/uploads/assets/celebrities/238-brad-pitt/1250530894_brad_pitt_290x402.jpg'];
+	$code_model[] = ['title' => 'Schedule Monster 2', 'link' => CODE_URL.'schedule_monster2/', 'release_date' => '06/09/2014', 'icon_url' => 'schedule-monster-icon.png'];
 	$code_model[] = ['title' => 'Game of Life', 'link' => CODE_URL.'game_of_life/', 'release_date' => '5/22/2014', 'icon_url' => 'game-of-life-icon.png'];
 	$code_model[] = ['title' => 'Mastermind', 'link' => CODE_URL.'mastermind/', 'release_date' => '04/12/2014', 'icon_url' => 'mastermind-icon.png'];
 
