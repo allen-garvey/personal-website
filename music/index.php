483e15a76f9b58bab75694105957562a48a2a861
Allen Garvey <allengarvey@hotmail.com>
2015-06-06 00:29:33 -0400

    Put iframe styles on music page into css to conform to html5

--- music/index.php
+++ music/index.php
@@ -17,7 +17,7 @@
 ?>
 
 <main class='container'>
-	<iframe width="100%" height="150" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/121888583&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+	<iframe class="music_player" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/121888583&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
 </main>
 
 
