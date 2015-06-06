53f856a9b54d7288df698357ac02b68ab58103bf
Allen Garvey <allengarvey@hotmail.com>
2015-06-06 02:24:04 -0400

    add artwork_image_url and artwork_image_thumbnail_url constants to reduce redundancy

--- inc/config.php
+++ inc/config.php
@@ -14,5 +14,8 @@ define("VIDEO_URL", BASE_URL.'video/');
 define("ARTWORK_URL", BASE_URL.'artwork/');
 define("MUSIC_URL", BASE_URL.'music/');
 
+define("ARTWORK_IMAGE_URL", IMAGE_URL.'artwork/');
+define("ARTWORK_IMAGE_THUMBNAIL_URL", IMAGE_URL.'artwork/thumbnails/');
+
 define("ARTWORK_DETAIL_URL", ARTWORK_URL.'index.php?art='); //for local development
 //define("ARTWORK_DETAIL_URL", ARTWORK_URL.'art/'); //for web pretty url
