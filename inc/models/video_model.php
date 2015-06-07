dc5618967fa847235810eea69734c62fef85379c
Allen Garvey <allengarvey@hotmail.com>
2015-06-07 00:49:53 -0400

    Rewrite descriptions for video clips

--- inc/models/video_model.php
+++ inc/models/video_model.php
@@ -2,16 +2,14 @@
 
 //model for video index page
 function videos(){
-	$videos = array();
-	$videos[] = array('title' => 'The Last Battle', 
+	$videos = [];
+	$videos[] = ['title' => 'The Last Battle', 
 						'release_date' => '01/28/2014',
 						'url' => 'http://www.youtube-nocookie.com/embed/WEsaplY1yco', 
-					'description' => 'A re-scoring of a clip from Final Fantasy XIV. This dramatic scene was both fun and challenging to score. 
-			It ended up being a showcase for my eclectic musical taste---I managed to use orchestral, funk, dubstep, downtempo, and a little polytonality all in the same track.');
-	$videos[] = array('title' => 'Marching Orders', 
+					'description' => "Always on the lookout for dramatic videos to score, I happened upon this trailer for <em>Final Fantasy XIV: A Realm Reborn.</em> While challenging, I had a lot of fun scoring this because of the many changes in action and mood. This ended up being a showcase for my eclectic musical taste---I managed to use orchestral, funk, dubstep, downtempo, and a bit of polytonality."];
+	$videos[] = ['title' => 'Marching Orders', 
 						'release_date' => '01/05/2014',
 						'url' => 'http://www.youtube-nocookie.com/embed/N67DgzuSpEQ', 
-						'description' => 'A re-scoring of the trailer for "The Edge of Tomorrow". 
-			This trailer was a perfect fit both subject and lengthwise for a dark, atmospheric, militaristic track I had already written, and so I was was able to fit them together with only slight modifications.');
+						'description' => "While playing around with a plug-in that emulated the old Speak &amp; Spell on a bassline, I was reminded of a military walkie talkie. After adding a boot stomp-like bass drum and some bells, I was well on my way to creating a dark, atmospheric, militaristic track. Shortly after I finished the piece I saw the trailer for <em>Edge of Tomorrow</em> and had a hunch that they would go well together. Indeed they did, as I was able to get everything to line up with only minor cutting. If only every was that easy!"];
 	return $videos;
 }
