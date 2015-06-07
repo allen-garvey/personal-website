f931a317c5c733c3a3b7764f1eddc2f734199ee3
Allen Garvey <allengarvey@hotmail.com>
2015-06-06 20:11:43 -0400

    add host_environment constant and if statements to config.php to more easily transfer it between local and production environments

--- inc/config.php
+++ inc/config.php
@@ -1,12 +1,17 @@
 <?php
 
-//******config to run on localhost
-define("BASE_URL","/Websites/allen_garvey_2/");
-define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/Websites/allen_garvey_2/");
+define("HOST_ENVIRONMENT", "local");
 
-//******config to run on namecheap
-// define("BASE_URL","http://allengarvey.com/");
-// define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/");
+if(HOST_ENVIRONMENT === 'production'){
+	//******config to run on namecheap
+	define("BASE_URL","http://allengarvey.com/");
+	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/");
+}
+else{
+	//******config to run on localhost
+	define("BASE_URL","/Websites/allen_garvey_2/");
+	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/Websites/allen_garvey_2/");	
+}
 
 define("IMAGE_URL", BASE_URL.'images/');
 define("CODE_URL", BASE_URL.'code/');
@@ -17,8 +22,11 @@ define("MUSIC_URL", BASE_URL.'music/');
 define("ARTWORK_IMAGE_URL", IMAGE_URL.'artwork/');
 define("ARTWORK_IMAGE_THUMBNAIL_URL", IMAGE_URL.'artwork/thumbnails/');
 
-//*******local development
-define("ARTWORK_DETAIL_URL", ARTWORK_URL.'index.php?art=');
-
-//*******namecheap
-//define("ARTWORK_DETAIL_URL", ARTWORK_URL.'art/'); //for web pretty url
+if(HOST_ENVIRONMENT === 'production'){
+	//*******namecheap
+	define("ARTWORK_DETAIL_URL", ARTWORK_URL.'art/'); //for web pretty url
+}
+else{
+	//*******local development
+	define("ARTWORK_DETAIL_URL", ARTWORK_URL.'index.php?art=');
+}
