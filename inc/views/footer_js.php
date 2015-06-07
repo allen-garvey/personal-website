b8bbcde04bb2c8896977b69d0a2fbdb9e1b6510f
Allen Garvey <allengarvey@hotmail.com>
2015-06-06 23:47:28 -0400

    make the echoing of extra_footer_scripts more clear by using if statement instead of ternary operator

--- inc/views/footer_js.php
+++ inc/views/footer_js.php
@@ -2,5 +2,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="<?= BASE_URL.'lib/bootstrap/bootstrap.min.js'?>"></script>
 <?php 
-$extra_footer_scripts = isset($extra_footer_scripts) ? $extra_footer_scripts : '';
-echo $extra_footer_scripts;
+if(isset($extra_footer_scripts)){
+	echo $extra_footer_scripts;	
+}
