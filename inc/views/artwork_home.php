82b8108c77f91d11fb4f1c14e54c8b2f5326133e
Allen Garvey <allengarvey@hotmail.com>
2015-06-08 20:34:49 -0400

    edit lead p copy for artwork page

--- inc/views/artwork_home.php
+++ inc/views/artwork_home.php
@@ -2,9 +2,9 @@
 	$section_header = [];
 	$section_header['title'] = 'Artwork';
 	$section_header['header_class'] = 'red_background';
-	$section_header['body'] = "In aesthetics class I learned that art can be representative, expressive, embody form, convey the art tradition, or engage your critical thinking. 
-								I think that all successful art should contain all these qualities, to some extent. 
-								As a graphic designer, I also always try to think about the appropriateness of the visual---for the subject, for the medium, and for the intended audience.";
+	$section_header['body'] = "While studying aesthetics I learned that art can be representative, expressive, embody form, convey the art tradition, or engage your critical thinking. 
+								I think all successful art contains these qualities, to some extent. 
+								As a graphic designer, I also always try to think about the appropriateness of the visual---for the subject, medium, message and the intended audience.";
 	$section_header['image'] = ['url' => 'portraits/allen-garvey-comic-book-portrait.png', 'alt' => 'Digital self-portrait of Allen Garvey as comic book super-hero'];
 	include(ROOT_PATH.'inc/views/section_header.php');
  ?>
