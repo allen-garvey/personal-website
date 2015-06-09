c62820a3b6624c72b81e8174d0aa448d19319d8d
Allen Garvey <allengarvey@hotmail.com>
2015-06-08 21:41:09 -0400

    Edit copy for about me on homepage

--- inc/models/homepage_model.php
+++ inc/models/homepage_model.php
@@ -3,11 +3,11 @@
 	function main_index_model(){
 		$model = [];
 
-		$model['main_text'] = "I'm a software developer, designer and musician. As far back as I can remember I've always been intensely curious, which leads me to never stop learning. I believe that if you do the work and put in the time for the things that you are most passionate about, good things will come to you.";
+		$model['main_text'] = "I'm a software developer, designer and musician. When you follow your passions like I have, you end up in some curious situations, like lugging an upright bass and amp across a university campus in a suit and tie, or programming Objective-C in the crew bar of a cruise ship at 3 o'clock in the afternoon. However, I believe if you're always creating, learning, and challenging yourself, the journey will be worth it.";
 
 		$model['accomplishment_list'] = ["wrote and produced an opera based on <em>Brave New World</em>",
 										"performed in over 50 countries on 4 continents",
-										"climbed Mt. Pahia in Bora Bora on three separate occasions",
+										"climbed Mt. Pahia in Bora Bora three times",
 										"taught web development, web design and Android app development to high school and college students",
 										"had artwork chosen for the <em>2015 Stacy M. Israel Art, Architecture + Design Student Exhibition</em> at Norwalk Community College"
 										];
