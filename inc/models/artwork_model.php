ceb5020a8d1e8dc771954972e50bdd0790b759d5
Allen Garvey <allengarvey@hotmail.com>
2015-06-10 23:10:19 -0400

    Add carousel images and captions to art detail model

--- inc/models/artwork_model.php
+++ inc/models/artwork_model.php
@@ -14,7 +14,13 @@ function artworks(){
 										'alt' => 'Self-portrait illustration of Allen Garvey as a classic comic book superhero', 
 										'description' => "<p>This illustration was created for a project in which we had to create an authentic-looking gold or silver-age comic book cover starring ourselves.
 This was a challenge both because of the historical accuracy required in the art style, type, color choices and layout, and because the simple art style of the time period leaves no place to hide errors. It was a further challenge for me because the comics I am both most familiar with and prefer the style of are the bronze age comics from the 80's and early 90's. While I do not think I completely nailed the historical accuracy part of the assignment, I was pleased with the final result and thought I did better adapting to the simpler style than I envisioned I would.</p>",
-										'orientation' => 'portrait'
+										'orientation' => 'portrait',
+										'carousel' => [
+											['image_url' => ARTWORK_IMAGE_URL.'allen-garvey-personal-comic-book.jpg', 'alt' => 'Self-portrait illustration of Allen Garvey as a classic comic book superhero'],
+											['image_url' => ARTWORK_IMAGE_URL.'allen-garvey-personal-comic-book1.jpg', 'alt' => 'Initial sketch of illustration of Allen Garvey as a classic comic book superhero', 'caption' => '<p>Initial sketch</p>'],
+											['image_url' => ARTWORK_IMAGE_URL.'allen-garvey-personal-comic-book2.jpg', 'alt' => 'Initial sketch of background for illustration of Allen Garvey as a classic comic book superhero', 'caption' => '<p>Background sketch with completed vector body</p>'],
+											['image_url' => ARTWORK_IMAGE_URL.'allen-garvey-personal-comic-book3.svg', 'alt' => 'Black and white illustration of Allen Garvey as a classic comic book superhero', 'caption' => '<p>Completed vector trace in black and white</p>']
+										]
 										];
 
 	$artworks['jk-simmons-illustration'] = ['title' => 'J.K. Simmons Celebrity Illustration', 
@@ -22,8 +28,14 @@ This was a challenge both because of the historical accuracy required in the art
 										'fullsize_extension' => '.png',
 										'thumbnail_extension' => '.jpg',
 										'alt' => 'Digital illustration of J.K. Simmons by Allen Garvey based on photo by Mark Mann', 
-										'description' => "<p>This picture was done for an assignment to create a recognizable illustration of a celebrity. I chose J.K. Simmons because I always enjoy his screen presence and acting skill, and because despite his recent Oscar I feel he is an underappreciated actor. Since he is equally talented at dramatic and comedic acting, early on I decided I wanted to use bold, contrasting colors to convey the wide breadth of characters he's portrayed. I started out by finding a photo that I thought captured his essence, which was taken by <a href='http://www.markmannphoto.com'>Mark Mann.</a> I then traced it using tracing paper and made a copy. On the copy I created smooth vector shapes over the soft pencil shading and scanned it in. I then traced the scan in Adobe Illustrator using black and white and shades of gray. Then I selected appropriate Pantone colors and used live paint to color it in.</p>",
-										'orientation' => 'portrait'
+										'description' => "<p>This picture was done for an assignment to create a recognizable illustration of a celebrity. I chose J.K. Simmons because I always enjoy his screen presence and acting skill, and because despite his recent Oscar I feel he is an underappreciated actor. Since he is equally talented at dramatic and comedic acting, early on I decided I wanted to use bold, contrasting colors to convey the wide breadth of characters he's portrayed. I started out by finding a photo that I thought captured his essence, which was taken by <a href='http://www.markmannphoto.com/PORTFOLIOS/A-list-B-list/4/caption'>Mark Mann.</a> I then traced it using tracing paper and made a copy. On the copy I created smooth vector shapes over the soft pencil shading and scanned it in. I then traced the scan in Adobe Illustrator using black and white and shades of gray. Then I selected appropriate Pantone colors and used live paint to color it in.</p>",
+										'orientation' => 'portrait',
+										'carousel' => [
+											['image_url' => ARTWORK_IMAGE_URL.'jk-simmons-illustration.png', 'alt' => 'Digital illustration of J.K. Simmons by Allen Garvey based on photo by Mark Mann'],
+											['image_url' => ARTWORK_IMAGE_URL.'jk-simmons-illustration1.jpg', 'alt' => 'Initial sketch for illustration of J.K. Simmons by Allen Garvey', 'caption' => '<p>Initial sketch</p>'],
+											['image_url' => ARTWORK_IMAGE_URL.'jk-simmons-illustration2.jpg', 'alt' => 'Refined sketch for illustration of J.K. Simmons by Allen Garvey', 'caption' => '<p>Refined sketch with smoother shapes for vector tracing</p>'],
+											['image_url' => ARTWORK_IMAGE_URL.'jk-simmons-illustration3.png', 'alt' => 'Black and white illustration of J.K. Simmons by Allen Garvey', 'caption' => '<p>Completed vector trace in black and white with top of the head added</p>']
+										]
 										];
 
 	$artworks['bitter-polar-bear'] = ['title' => 'Bitter Polar Bear', 
@@ -31,11 +43,19 @@ This was a challenge both because of the historical accuracy required in the art
 										'fullsize_extension' => '.png',
 										'thumbnail_extension' => '.jpg',
 										'alt' => 'Digital illustration of a bitter polar bear by Allen Garvey based on photo by Mike Reyfman', 
-										'description' => "<p>Inspired by the phrase bitter polar bear which popped up in a random word generator, I wanted to imagine what that would look like. After some exploratory sketches, I decided on an image somewhere between a bear growling and a baby bawling. Starting out with a polar bear photo I found by <a href='http://www.mikereyfman.com'>Mike Reyfman</a>, I made a series of sketches. I took the better half of one of the sketches, I reversed it in Photoshop to create a symmetrical drawing. I then further refined the sketch, adding more detail and asymmetry. I alternated between scanning in and tracing the sketch in Adobe Illustrator and printing it out and drawing on the printout until I was satisfied with the level of detail. I then used live paint in Illustrator to color the illustration, using tints and shades of Pantone colors.</p>",
-										'orientation' => 'portrait'
+										'description' => "<p>Inspired by the phrase bitter polar bear which popped up in a random word generator, I wanted to imagine what that would look like. After some exploratory sketches, I decided on an image somewhere between a bear growling and a baby bawling. Starting out with a polar bear photo I found by <a href='http://www.mikereyfman.com/photo/gallery.php?Gallery=Polar-Bears-Svalbard-Spitsbergen-Norway'>Mike Reyfman</a>, I made a series of sketches. I took the better half of one of the sketches, I reversed it in Photoshop to create a symmetrical drawing. I then further refined the sketch, adding more detail and asymmetry. I alternated between scanning in and tracing the sketch in Adobe Illustrator and printing it out and drawing on the printout until I was satisfied with the level of detail. I then used live paint in Illustrator to color the illustration, using tints and shades of Pantone colors.</p>",
+										'orientation' => 'portrait',
+										'carousel' => [
+											['image_url' => ARTWORK_IMAGE_URL.'bitter-polar-bear.png', 'alt' => 'Digital illustration of a bitter polar bear by Allen Garvey based on photo by Mike Reyfman'],
+											['image_url' => ARTWORK_IMAGE_URL.'bitter-polar-bear1.jpg', 'alt' => 'Pen outline of initial sketch of a bitter polar bear by Allen Garvey', 'caption' => '<p>Initial sketch outlined in pen</p>'],
+											['image_url' => ARTWORK_IMAGE_URL.'bitter-polar-bear2.jpg', 'alt' => 'Refined sketch for illustration of J.K. Simmons by Allen Garvey', 'caption' => '<p>Right half reversed to create symmetrical drawing</p>'],
+											['image_url' => ARTWORK_IMAGE_URL.'bitter-polar-bear3.jpg', 'alt' => 'Smooth pen outline for illustration of J.K. Simmons by Allen Garvey', 'caption' => '<p>Smoother pen outline with asymmetry added</p>'],
+											['image_url' => ARTWORK_IMAGE_URL.'bitter-polar-bear4.jpg', 'alt' => 'Further refined sketch for illustration of J.K. Simmons by Allen Garvey', 'caption' => '<p>More detail sketched over vector printout</p>']
+										
+										]
 										];
 
-	$artworks['think-logo'] = ['title' => 'Think Logo', 
+	$artworks['think-logo'] = ['title' => 'Think Wordmark', 
 										'date_completed' => '12/14/2014',
 										'fullsize_extension' => '.svg',
 										'thumbnail_extension' => '.svg', 
@@ -47,7 +67,12 @@ This was a challenge both because of the historical accuracy required in the art
 														After much sketching, I decided to use a slab serif font for the 'INK' part of the logo as a contrast to the thin Didot based 'th'.</p>
 														<p>The next part of the process was to translate my sketches in Adobe Illustrator. I started out with the letters th in Didot and the letters INK in the font Rockwell. I then proceeded to alter the letterforms to conform to my sketches. I decided to use different shades of midnight blue color to the letters 'INK' in increasing value. While I wanted the logo to be mostly black and white due to my feeling that that looks more sophisticated, I felt that the slight color added subtlety to the logo, as well as having connotations of pen or tattoo ink.</p>
 														<p>After looking over my final design, I think the ascending letter heights draw your eye from left to right, and the middle asterisk adds balance to the design, as well as creating possibilities for it to be used as a stand-alone mark or symbol. The uneven letter heights also create a sense of interest and make the logo more distinctive.</p>",
-										'orientation' => 'portrait'
+										'orientation' => 'portrait',
+										'carousel' => [
+											['image_url' => ARTWORK_IMAGE_URL.'think-logo.svg', 'alt' => 'Wordmark for think, a sophisticated urban tattoo shop, by Allen Garvey'],
+											['image_url' => ARTWORK_IMAGE_URL.'think-logo1.jpg', 'alt' => 'Initial explorations for think wordmark', 'caption' => '<p>Initial explorations</p>'],
+											['image_url' => ARTWORK_IMAGE_URL.'think-logo2.png', 'alt' => 'Alternate candidates for think logo', 'caption' => '<p>Alternative candidates for the mark</p>']
+										]
 										];
 
 	$artworks['djupivogur-mountain-scene'] = ['title' => 'Djúpivogur Mountain Scene', 
