d6363c2643a615f6c5e2c82c5f2887159cee0413
Allen Garvey <allengarvey@hotmail.com>
2015-06-13 16:22:45 -0400

    Get rid of bootstrap-default theme from stylesheets in view files

--- inc/views/head_only.php
+++ inc/views/head_only.php
@@ -1,10 +1,7 @@
-<?php 
-$additional_stylesheets = isset($additional_stylesheets) ? $additional_stylesheets : '';
- ?>
 <!DOCTYPE html><html><head>
 <meta charset='utf-8' />
 <title>Allen Garvey | Software Developer, Designer, Musician</title>
-<?php include(ROOT_PATH . 'inc/views/bootstrap.php'); ?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"/>
 <link href='http://fonts.googleapis.com/css?family=Yellowtail&amp;text=AlenGarvy' rel='stylesheet' type='text/css'/>
 <link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'/>
 <link rel="stylesheet" type="text/css" href="<?= BASE_URL.'stylesheets/master.css'; ?>">
@@ -12,5 +9,9 @@ $additional_stylesheets = isset($additional_stylesheets) ? $additional_styleshee
 <meta name="keywords" content="Allen Garvey Software Developer Programmer Graphic Sound Designer Musician Bassist Composer"/>
 <meta name="author" content="Allen Garvey"/>
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-<?= $additional_stylesheets; ?>
+<?php 
+	if(isset($additional_stylesheets)){
+		echo $additional_stylesheets;
+	}
+?>
 </head>
