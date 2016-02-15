6e1ca753356bd9dd057ae371e43127153de2be30
Allen Garvey <allengarvey@hotmail.com>
2016-02-14 21:17:00 -0500

    add bootstrap.min.js

--- inc/views/footer_js.php
+++ inc/views/footer_js.php
@@ -1,6 +1,6 @@
 <?php //javascript required for bootstrap navigation to work ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="<?= BASE_URL.'lib/bootstrap/bootstrap.min.js'?>"></script>
+<script src="<?= SCRIPTS_URL.'bootstrap.min.js'?>"></script>
 <?php 
 if(isset($extra_footer_scripts)){
 	echo $extra_footer_scripts;	
