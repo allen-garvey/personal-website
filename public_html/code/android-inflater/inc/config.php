ecf33c7a7dde28a65724150fb1f89dd693279af0
Allen Garvey <allengarvey@hotmail.com>
2016-02-15 19:15:02 -0500

    remove android inflator files since it is now being served on githhub pages

--- public_html/code/android-inflater/inc/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//******local
-// define("BASE_URL","/android-inflater/");
-// define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/Websites/allen_garvey_2/code/android-inflater/");
-
-//****namecheap web hosting
-define("BASE_URL","http://www.allengarvey.com/code/android-inflater/");
-define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/code/android-inflater/");
